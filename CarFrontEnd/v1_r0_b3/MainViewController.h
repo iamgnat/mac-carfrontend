639641bd2073cf7a89dd9603c4f14163532000a2
iamgnat <iamgnat@360743b7-b936-0410-8cf8-11b756d0386d>
2008-03-15 03:35:41 +0000

    tag migration

---
+++ CarFrontEnd/v1_r0_b3/MainViewController.h
@@ -0,0 +1,76 @@
+/*
+ * CarFrontEnd - MainViewController.h - David Whittle (iamgnat@gmail.com)
+ * Copyright (C) 2007  David Whittle (iamgnat@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import <Cocoa/Cocoa.h>
+#import <ApplicationServices/ApplicationServices.h>
+#import <CarFrontEndAPI/CarFrontEndAPI.h>
+
+@class AudioVolumeManager;
+@class PluginManager;
+@class SystemManager;
+
+@interface MainViewController : NSObject {
+    IBOutlet AudioVolumeManager *audioVolumeManager;
+    IBOutlet PluginManager      *pluginManager;
+    IBOutlet SystemManager      *systemManager;
+    
+    IBOutlet NSTextField        *dateTextField;
+    
+    IBOutlet NSView             *contentView;
+    IBOutlet NSView             *mainView;
+    IBOutlet NSView             *splashView;
+        
+    // Other
+    NSString                    *appSupportPath;
+    NSView                      *contentViewContent;
+    NSWindow                    *mainWindow;
+    NSMutableDictionary         *prefsConfig;
+    NSString                    *prefsConfigPath;
+    
+    // Resolution change
+    NSDictionary                *currentDisplayMode;
+    NSDictionary                *cfeDisplayMode;
+}
+
+#pragma mark File's Owner Delegates
+- (void) applicationDidFinishLaunching: (NSNotification *) notification;
+- (void) applicationWillTerminate: (NSNotification *) notification;
+- (void) applicationWillHide: (NSNotification *) notification;
+- (void) applicationDidUnhide: (NSNotification *) notification;
+
+#pragma mark Manage Content View
+- (void) replaceContentWith:(NSView *) view;
+
+#pragma mark Utility Methods
+- (void) updateDateTime: (id) ignored;
+- (void) changeDisplayTo: (NSDictionary *) mode;
+- (NSRect) mainWindowFrame;
+- (int) mainWindowLevel;
+- (NSString *) appSupportPath;
+- (NSRect) contentViewFrame;
+- (BOOL) swapDriverToSide: (NSString *) side;
+
+#pragma mark AppleScript Utilities
+- (NSAppleEventDescriptor *) runWithSource: (id) source
+                       andReturnError: (NSDictionary **) error;
+
+#pragma mark Preferences configInfo
+- (NSDictionary *) preferencesForKey: (NSString *) key;
+- (void) setPreferences: (NSDictionary *) prefs forKey: (NSString *) key;
+
+@end
